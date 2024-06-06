878c8cde6ba0bff84b0194d724f5fbdb91c4d345
SalvaPeris <sperisgimeno@gmail.com>
2024-06-06 15:54:28 +0200

    Added GraphQL types.

---
+++ GraphQL.WebApi/GraphQL/Types/ReviewObject.cs
@@ -0,0 +1,26 @@
+﻿using GraphQL.Types;
+using GraphQL.WebApi.Models;
+
+namespace GraphQL.WebApi.GraphQL.Types
+{
+    public sealed class ReviewObject : ObjectGraphType<Review>
+    {
+        public ReviewObject()
+        {
+            Name = nameof(Review);
+            Description = "A review of the movie";
+
+            Field(r => r.Id, type: typeof(IntGraphType))
+                .Description("review Id");
+
+            Field(r => r.MovieId, type: typeof(GuidGraphType))
+                .Description("Guid Id of the movie");
+
+            Field(r => r.Reviewer)
+                .Description("Name of the reviewer");
+
+            Field(r => r.Stars)
+                .Description("Star rating out of five");
+        }
+    }
+}

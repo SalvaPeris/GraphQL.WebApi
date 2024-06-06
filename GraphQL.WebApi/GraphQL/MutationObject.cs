04a7939de68e19ed80570c48cd7d0a99dfa2aaa8
SalvaPeris <sperisgimeno@gmail.com>
2024-06-06 15:58:52 +0200

    Added GraphQL Query and Mutation.

---
+++ GraphQL.WebApi/GraphQL/MutationObject.cs
@@ -0,0 +1,44 @@
+﻿using GraphQL.Resolvers;
+using GraphQL.Types;
+using GraphQL.WebApi.Data.Repositories;
+using GraphQL.WebApi.GraphQL.Types;
+using GraphQL.WebApi.Models;
+using System.Xml.Linq;
+
+namespace GraphQL.WebApi.GraphQL
+{
+    public class MutationObject : ObjectGraphType<object>
+    {
+        public MutationObject(IMovieRepository repository)
+        {
+            Name = "Mutations";
+            Description = "The base mutation for all the entities in our object graph.";
+
+            AddField(new FieldType
+            {
+                Name = "addReview",
+                Description = "Add review to a movie.",
+                Type = typeof(MovieObject),
+                Arguments = new QueryArguments
+                (
+                    new QueryArgument<NonNullGraphType<GuidGraphType>>
+                    {
+                        Name = "id",
+                        Description = "The unique GUID of the movie."
+                    },
+                    new QueryArgument<NonNullGraphType<ReviewInputObject>>
+                    {
+                        Name = "review",
+                        Description = "Review for the movie"
+                    }
+                ),
+                Resolver = new FuncFieldResolver<object>(context =>
+                {
+                    var id = context.GetArgument<Guid>("id");
+                    var review = context.GetArgument<Review>("review");
+                    return repository.AddReviewToMovieAsync(id, review);
+                })
+            });
+        }
+    }
+}
---
+++ GraphQL.WebApi/GraphQL/QueryObject.cs
@@ -0,0 +1,36 @@
+﻿using GraphQL.Resolvers;
+using GraphQL.Types;
+using GraphQL.WebApi.Data.Repositories;
+using GraphQL.WebApi.GraphQL.Types;
+
+namespace GraphQL.WebApi.GraphQL
+{
+    public class QueryObject : ObjectGraphType<object>
+    {
+        public QueryObject(IMovieRepository movieRepository)
+        {
+            AddField(new FieldType
+            {
+                Name = "movies",
+                Type = typeof(ListGraphType<MovieObject>),
+                Resolver = new FuncFieldResolver<object>(async context =>
+                {
+                    return await movieRepository.GetMoviesAsync();
+                })
+            });
+
+            AddField(new FieldType
+            {
+                Name = "movie",
+                Type = typeof(MovieObject),
+                Arguments = new QueryArguments(
+                    new QueryArgument<NonNullGraphType<GuidGraphType>> { Name = "id" }
+                ),
+                Resolver = new FuncFieldResolver<object>(async context =>
+                {
+                    return await movieRepository.GetMovieByIdAsync(context.GetArgument("id", Guid.Empty));
+                })
+            });
+        }
+    }
+}

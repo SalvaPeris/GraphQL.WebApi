78b5e708bfda357b5c6787d5581ab88c1497aff8
SalvaPeris <sperisgimeno@gmail.com>
2024-06-06 15:51:13 +0200

    All done before integrating GraphQL.

---
+++ GraphQL.WebApi/Data/Repositories/IMovieRepository.cs
@@ -0,0 +1,11 @@
+﻿using GraphQL.WebApi.Models;
+
+namespace GraphQL.WebApi.Data.Repositories
+{
+    public interface IMovieRepository
+    {
+        Task<List<Movie>> GetMoviesAsync();
+        Task<Movie> GetMovieByIdAsync(Guid id);
+        Task<Movie> AddReviewToMovieAsync(Guid id, Review review);
+    }
+}
---
+++ GraphQL.WebApi/Data/Repositories/MovieRepository.cs
@@ -0,0 +1,44 @@
+﻿using GraphQL.WebApi.Data.Context;
+using GraphQL.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQL.WebApi.Data.Repositories
+{
+    public class MovieRepository : IMovieRepository
+    {
+        private readonly MovieContext _context;
+
+        public MovieRepository(MovieContext context)
+        {
+            _context = context;
+            _context.Database.EnsureCreated();
+        }
+
+        public async Task<List<Movie>> GetMoviesAsync()
+        {
+            return await _context.Movies
+                                  .AsNoTracking()
+                                  .ToListAsync();
+        }
+
+        public async Task<Movie> GetMovieByIdAsync(Guid id)
+        {
+            return await _context.Movies
+                                  .Where(m => m.Id == id)
+                                  .AsNoTracking()
+                                  .FirstOrDefaultAsync();
+        }
+
+        public async Task<Movie> AddReviewToMovieAsync(Guid id, Review review)
+        {
+            var movie = await _context.Movies
+                                  .Where(m => m.Id == id)
+                                  .FirstOrDefaultAsync();
+
+            movie.AddReview(review);
+
+            await _context.SaveChangesAsync();
+            return movie;
+        }
+    }
+}

d4669f9a7da8cd290c670ed0faef498e77fb0894
SalvaPeris <sperisgimeno@gmail.com>
2024-06-06 16:51:32 +0200

    Added Schema and Altair UI.

---
+++ GraphQL.WebApi/GraphQL/MovieReviewSchema.cs
@@ -0,0 +1,13 @@
+﻿using GraphQL.Types;
+
+namespace GraphQL.WebApi.GraphQL
+{
+    public class MovieReviewSchema : Schema
+    {
+        public MovieReviewSchema(QueryObject query, MutationObject mutation, IServiceProvider sp) : base(sp)
+        {
+            Query = query;
+            Mutation = mutation;
+        }
+    }
+}
--- GraphQL.WebApi/Program.cs
+++ GraphQL.WebApi/Program.cs
@@ -1,18 +1,42 @@
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using GraphQL;
 using GraphQL.WebApi.Data.Context;
+using GraphQL.WebApi.Data.Repositories;
+using GraphQL.WebApi.GraphQL;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+           .ConfigureContainer<ContainerBuilder>(containerBuilder =>
+           {
+               containerBuilder.RegisterType<MovieRepository>().As<IMovieRepository>().InstancePerLifetimeScope();
+               containerBuilder.RegisterType<QueryObject>().AsSelf().SingleInstance().UsingConstructor(typeof(IMovieRepository));
+               containerBuilder.RegisterType<MutationObject>().AsSelf().SingleInstance().UsingConstructor(typeof(IMovieRepository));
+           });
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services
        .AddEntityFrameworkInMemoryDatabase()
        .AddDbContext<MovieContext>(context => { context.UseInMemoryDatabase("MovieDb"); });
 
+builder.Services.AddScoped<IMovieRepository, MovieRepository>()
+    .AddGraphQL(options =>
+    {
+        options.AddErrorInfoProvider(o => o.ExposeExceptionDetails = builder.Environment.IsDevelopment());
+        options.AddSchema<MovieReviewSchema>();
+        options.AddGraphTypes(Assembly.GetExecutingAssembly());
+        options.AddDataLoader();
+        options.AddSystemTextJson();
+    });
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -20,6 +44,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseGraphQL();
+app.UseGraphQLAltair();
 
 app.UseAuthorization();
 
